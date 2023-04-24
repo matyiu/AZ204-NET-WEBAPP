2cf5ca258e70fe7698789e60221a5f938ea87222
Jefferson Valle <valleespinozajefferson@gmail.com>
2023-04-24 11:06:40 +0200

    Create page for high CPU Usage

---
+++ Pages/CPULoad.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AZ_204_WindowsWebApp.Pages
+{
+    public class CPULoadModel: PageModel
+    {
+        public long counter = 0;
+        public long elapsedMs;
+
+        public void OnGet()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            while (counter < 1000000000000000)
+            {
+                counter = counter + 1;
+            }
+
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+        }
+    }
+}
